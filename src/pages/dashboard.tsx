b49fd5dfc6c0f821f5ff57bb7976309d39d592fe
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-08 16:58:26 -0300

    feature: inicio da sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/header";
+import { SideBar } from "../components/sideBar";
 
 export default function Dashboard() {
-  return <Header />
+  return (
+    <Flex flexDir='column'h='100vh'>
+      <Header />
+
+      <Flex w='100%' maxW={1480} mx='auto' px='6' my='6' >
+        <SideBar />
+      </Flex>
+    </Flex>
+  )
 }
