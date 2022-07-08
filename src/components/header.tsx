234e626373fb99a28066f9f6b4beaafc478ffc50
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-08 10:46:55 -0300

    feature: finaliza header da aplicacao

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react"
-import { RiSearchLine } from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react"
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export const Header = () => {
   return (
@@ -17,6 +17,21 @@ export const Header = () => {
       <Flex as='label' flex='1' align='center' py='4' px='8' ml='6' bg='gray.800' color='gray.200' position='relative' borderRadius='full' maxW={400}>
         <Input placeholder="Buscar na Plataforma" _placeholder={{ color: 'gray.400' }} color='gray.50' px='4' mr='4' variant='unstyled' />
         <Icon as={RiSearchLine} fontSize='20' />
+      </Flex>
+
+      <Flex align='center' ml='auto'>
+        <HStack spacing='8' color='gray.400' mx='8' py='1' pr='8' borderRightWidth='1px' borderColor='gray.400'>
+          <Icon as={RiNotificationLine} fontSize='20' />
+          <Icon as={RiUserAddLine} fontSize='20' />
+        </HStack>
+
+        <Flex align='center'>
+          <Box textAlign='right' mr='4'>
+            <Text>Thiago Nunes</Text>
+            <Text color='gray.400'>desenvolvedor.nunes@gmail.com</Text>
+          </Box>
+          <Avatar size='md' name="Thiago Nunes" src="https://github.com/thg-nunes.png" />
+        </Flex>
       </Flex>
     </Flex>
   )
