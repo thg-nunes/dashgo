5cf986a94fb62576aa1b82f8a8a3e181cf91e431
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-11 07:20:56 -0300

    feature: realiza validacao de dados

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -1,12 +1,14 @@
 import { forwardRef, ForwardRefRenderFunction } from 'react'
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react"
+import { FieldError } from 'react-hook-form'
 
 interface InputProps extends ChakraInputProps {
   name: string
   label?: string
+  error?: FieldError
 }
 
-const InputBase:ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
+const InputBase:ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, error, ...rest }, ref) => {
   return (
     <FormControl>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
@@ -18,6 +20,7 @@ const InputBase:ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name
         variant={'filled'}
         bg={'gray.900'}
         ref={ref}
+        error={error}
         _hover={{bg: 'gray-900'}}
         {...rest}
       />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import Head from 'next/head'
 import type { NextPage } from 'next'
 import { Button, Flex, Stack } from '@chakra-ui/react'
-import { Input } from '../components/form/input'
-
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { Input } from '../components/form/input'
 
 type SignFormData = {
   email: string
   password: string
 }
 
+const signinSchema = yup.object().shape({
+  email: yup.string().required().email('E-mail inválido.'),
+  password: yup.string().required()
+})
+
 const SignIn: NextPage = () => {
-  const { register, handleSubmit, formState } = useForm()
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signinSchema)
+  })
 
   async function handleForm(data: SignFormData) {
     await new Promise(resolve => setTimeout(resolve , 2000))
@@ -26,8 +35,8 @@ const SignIn: NextPage = () => {
       <Flex w='100vw' h='100vh' align='center' justify='center'>
         <Flex as={'form'} w='100%' maxW={360} borderRadius={8} bg='gray.800' p='8' flexDir={'column'} onSubmit={handleSubmit(handleForm)}>
           <Stack spacing={'4'}>
-            <Input id='email' type={'email'} label='E-mail' {...register('email')} />
-            <Input id='password' type={'password'} label='Senha' {...register('password')} />
+            <Input id='email' type={'email'} label='E-mail' error={{type: 'required', message: 'E-mail obrigatório.'}} {...register('email')} />
+            <Input id='password' type={'password'} label='Senha' error={{type: 'required', message: 'Senha obrigatória.'}} {...register('password')} />
           </Stack>
 
           <Button mt={'6'} colorScheme={'pink'} size={'lg'} type='submit' isLoading={formState.isSubmitting}>Entrar</Button>
