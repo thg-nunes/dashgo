705fb5cb0f30a21d4f6fdfcb6a87a2e46cca4004
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-14 09:23:48 -0300

    feature: cria logica para botoes de paginacao

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,6 +1,16 @@
-import { Box, Button, Flex, Stack } from "@chakra-ui/react"
+import { useState } from "react"
+import { Box, Flex, Stack, Text } from "@chakra-ui/react"
+import { PaginationItem } from "./paginationItem"
 
 export const Pagination = () => {
+  let [pageActive, setPageActive] = useState(1)
+  const totalPages = [...new Array(40)].map((_, index) => index + 1)
+  const firstPage = 1
+  const lastPage = totalPages.length
+  const init_slice = pageActive === 1 ? 0 : (pageActive - 2)
+  const end_slice = pageActive === 1 ? pageActive + 2 : pageActive + 1
+  const buttonsPageVisible = totalPages.slice((init_slice), (end_slice))
+
   return (
     <Flex justify={'space-between'} pt='8' align={'center'} flexDir={['column', 'row']}>
       <Box>
@@ -8,12 +18,34 @@ export const Pagination = () => {
       </Box>
       <Box>
         <Stack spacing={'4'} flexDir='row' align={'center'} direction='row' mt={['4', '0']}>
-          <Button borderRadius={8} fontSize='sm' bg='gray.500' _hover={{bg: 'gray.300'}} colorScheme='pink' disabled _disabled={{
-            bg: 'pink.500', cursor: 'default'
-          }}>1</Button>
-          <Button borderRadius={8} fontSize='sm' bg='gray.500' _hover={{bg: 'gray.300'}} color='whiteAlpha.700'>2</Button>
-          <Button borderRadius={8} fontSize='sm' bg='gray.500' _hover={{bg: 'gray.300'}} color='whiteAlpha.700'>3</Button>
-        </Stack>
+          <Flex>
+            {(firstPage + 2) < pageActive && (
+              <>
+                <PaginationItem number={1} onPageChange={() => setPageActive(1)} />
+                <Text w='8' align='center'>...</Text>
+              </>
+            )}
+
+            <Stack spacing={'4'} flexDir='row' align={'center'} direction='row' mt={['4', '0']}>
+              {buttonsPageVisible.map(page => (
+                  <PaginationItem
+                    onPageChange={setPageActive}
+                    key={page}
+                    number={page}
+                    isCurrent={page === pageActive ? true : false}
+                  />
+                ))
+              }
+            </Stack>
+
+            {pageActive + 2 < lastPage && (
+              <>
+                <Text w='8' align='center'>...</Text>
+                <PaginationItem number={40} onPageChange={() => setPageActive(40)} />
+              </>
+            )}
+          </Flex>
+      </Stack>
       </Box>
     </Flex>
   )
